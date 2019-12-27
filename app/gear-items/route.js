ef00a8edfc4cb796696ef06c9371d5ddba5b1532
Evan Butler <evanb447@gmail.com>
2019-12-27 15:18:16 -0700

    [feature] find and display user's gearItems

--- app/gear-items/route.js
+++ app/gear-items/route.js
@@ -1,13 +1,20 @@
 import Route from '@ember/routing/route'
+import { inject as service } from '@ember/service'
 
 export default class GearClosetRoute extends Route {
-  model() {
-    return [
-      {
-        id: 0,
-        name: 'Borah Tarp',
-        weight: '',
-      },
-    ]
+  @service firebaseApp
+
+  async model() {
+    const {
+      currentUser: { uid },
+    } = await this.firebaseApp.auth()
+
+    if (!uid) {
+      return this.transitionTo('/')
+    }
+
+    return await this.store.query('gearItems', {
+      where: ['userId', '==', uid],
+    })
   }
 }
