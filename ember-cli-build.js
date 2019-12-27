209c8773831e9398172744a3e6aea43f9738ef2f
Evan Butler <evanb447@gmail.com>
2019-12-27 11:23:39 -0700

    [chore] installs @babel/plugin-proposal-optional-chaining

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,7 +6,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app')
 
 module.exports = function(defaults) {
   const app = new EmberApp(defaults, {
-    // Add options here
+    babel: {
+      plugins: ['@babel/plugin-proposal-optional-chaining'],
+    },
   })
 
   // Use `app.import` to add additional libraries to the generated
