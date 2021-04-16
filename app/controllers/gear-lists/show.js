97520a618f94c85d19933393b60b656ce38a6d0e
Evan Butler <evanb447@gmail.com>
2021-04-16 11:30:16 -0700

    add a group to a GearList

--- app/controllers/gear-lists/show.js
+++ app/controllers/gear-lists/show.js
@@ -2,4 +2,23 @@ import Controller from '@ember/controller'
 
 export default class GearListsShowController extends Controller {
   save = () => this.model.save()
+
+  addGroup = () => {
+    this.model.groups.push({
+      name: 'New Group',
+      description: '',
+      gearListItems: [
+        {
+          gearListItem: {
+            name: '',
+            description: '',
+            weight: '',
+          },
+          quantity: 1,
+        },
+      ],
+    })
+
+    this.model.save()
+  }
 }
