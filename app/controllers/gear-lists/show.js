49e040cca56038837421cebe6c049f1b402eb7ea
Evan Butler <evanb447@gmail.com>
2021-04-18 23:00:43 -0700

    remove a group, add an item

--- app/controllers/gear-lists/show.js
+++ app/controllers/gear-lists/show.js
@@ -1,24 +1,34 @@
 import Controller from '@ember/controller'
 
 export default class GearListsShowController extends Controller {
-  save = () => this.model.save()
+  save = () => this.model.gearList.save()
 
   addGroup = () => {
-    this.model.groups.push({
+    this.model.gearList.groups.push({
       name: 'New Group',
       description: '',
       gearListItems: [
         {
-          gearListItem: {
-            name: '',
-            description: '',
-            weight: '',
-          },
+          gearItem: null,
           quantity: 1,
         },
       ],
     })
 
-    this.model.save()
+    this.model.gearList.save()
+  }
+
+  removeGroup = async (groupIndex) => {
+    this.model.gearList.groups = [
+      ...this.model.gearList.groups.filter((_, i) => i !== groupIndex),
+    ]
+    this.model.gearList.save()
+  }
+
+  addGearItem = (groupIndex) => {
+    this.model.gearList.groups[groupIndex].gearListItems.push({
+      gearItem: null,
+      quantity: 1,
+    })
   }
 }
--- app/routes/gear-lists/show.js
+++ app/routes/gear-lists/show.js
@@ -1,10 +1,14 @@
 import Route from '@ember/routing/route'
 import { inject as service } from '@ember/service'
+import { hash } from 'rsvp'
 
 export default class GearListsShowRoute extends Route {
   @service store
 
   model({ gear_list_id }) {
-    return this.store.findRecord('gearList', gear_list_id)
+    return hash({
+      gearList: this.store.findRecord('gearList', gear_list_id),
+      gearItems: this.store.query('gearItem', { subscribe: true }),
+    })
   }
 }
