d7c3eb575a51e3665728812ebd5c971281d12434
Evan Butler <evanb447@gmail.com>
2021-04-30 15:44:19 -0700

    adds fields to GearItem model

--- app/models/gear-item.d.ts
+++ app/models/gear-item.d.ts
@@ -1,8 +1,10 @@
 interface GearItem {
   name: string;
   description: string;
-  units: string;
+  units: "g" | "oz" | "kg" | "lb";
   weight: string;
+  worn: boolean;
+  consumable: boolean;
 }
 
 export default GearItem
